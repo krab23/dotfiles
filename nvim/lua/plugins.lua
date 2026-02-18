7e27d82270ce33fe1c5539f5ab98f61044bfba69
Kenji <krab23@gmail.com>
2026-02-17 22:40:45 -0300

    fixes to init.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,203 +1,254 @@
 return {
-  --colorschemes
+  -- Colorschemes
   {
     "catppuccin/nvim",
-    lazy = false,
     name = "catppuccin",
+    lazy = false,
     priority = 1000,
     config = function()
       require("catppuccin").setup({
         flavour = "frappe",
-        transparent_background = true})
-        vim.cmd.colorscheme "catppuccin-frappe"
-    end
+        transparent_background = true,
+      })
+      vim.cmd.colorscheme("catppuccin-frappe")
+    end,
   },
   {
     "rebelot/kanagawa.nvim",
-    lazy = false,
     name = "kanagawa",
-    priority = 1000,
-    config = function()
-      require("kanagawa").setup({
-        theme = "dragon",
-        transparent = true})
-        --vim.cmd.colorscheme "kanagawa-dragon"
-    end
+    lazy = true,
   },
 
-
-  --vim be good
+  -- Practice / Utilities
   {
     "ThePrimeagen/vim-be-good",
-    lazy=true
+    cmd = "VimBeGood",
   },
-
-
-  --utilities
   {
     "nvim-tree/nvim-web-devicons",
-    lazy = false,
+    lazy = true,
   },
   {
     "nvim-tree/nvim-tree.lua",
-    lazy = false,
+    cmd = { "NvimTreeOpen", "NvimTreeToggle", "NvimTreeFocus" },
+    keys = {
+      { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle file tree" },
+    },
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
     config = function()
       require("nvim-tree").setup({})
-    end
+    end,
   },
   {
     "nvim-lualine/lualine.nvim",
-    lazy=false,
+    event = "VeryLazy",
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
     config = function()
       require("lualine").setup({
         options = {
-          component_separators = {left = '⧽', right = '⧼'},
-          section_separators = {left = '', right = ''},
-          --Separator Options: 
-          }
+          component_separators = { left = "⧽", right = "⧼" },
+          section_separators = { left = "", right = "" },
+        },
       })
-    end
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
-    lazy = false,
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
     config = function()
       require("nvim-treesitter.configs").setup({
         highlight = {
           enable = true,
           additional_vim_regex_highlight = false,
-        }
+        },
       })
-    end
+    end,
   },
   {
     "nvim-telescope/telescope.nvim",
-    lazy = false,
+    cmd = "Telescope",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-lua/popup.nvim",
     },
-    vim.keymap.set("n","<leader>ff",":Telescope find_files<CR>",{}),
-    vim.keymap.set("n","<leader>fg",":Telescope live_grep<CR>",{}),
-    vim.keymap.set("n","<leader>fb",":Telescope buffers<CR>",{}),
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Find buffers" },
+    },
   },
   {
     "folke/which-key.nvim",
-    lazy = true
+    event = "VeryLazy",
+    config = function()
+      require("which-key").setup({})
+    end,
   },
   {
     "github/copilot.vim",
-    lazy = false
+    event = "InsertEnter",
   },
-  --lsp
+
+  -- LSP
   {
     "williamboman/mason.nvim",
-    lazy = false,
-    event = "BufReadPre",
+    cmd = { "Mason", "MasonInstall", "MasonUpdate" },
+    build = ":MasonUpdate",
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    event = "BufReadPre",
-    dependencies = {"williamboman/mason.nvim"},
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
     config = function()
       require("mason-lspconfig").setup({
-        automatic_installation = true
+        automatic_installation = true,
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
-    lazy = false,
-    dependencies = {"williamboman/mason-lspconfig.nvim"},
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
---      require("lspconfig").lua_ls.setup({
---        settings = {
---          Lua = {
---            diagnostics = {
---              globals = {
---                "vim"
---              },
---            },
---            workspace = {
---              library = {
---                [vim.fn.expand("$VIMRUNTIME0/lua")] = true,
---                [vim.fn.stdpath("config") .. "/lua"] = true,
---              },
---            },
---          }
---        }
---     })
-    end
+      local lspconfig = require("lspconfig")
+      local mason_lspconfig = require("mason-lspconfig")
+
+      local default_capabilities = vim.lsp.protocol.make_client_capabilities()
+
+      mason_lspconfig.setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = default_capabilities,
+          })
+        end,
+        ["lua_ls"] = function()
+          lspconfig.lua_ls.setup({
+            capabilities = default_capabilities,
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" },
+                },
+                workspace = {
+                  library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.stdpath("config") .. "/lua"] = true,
+                  },
+                },
+              },
+            },
+          })
+        end,
+      })
+    end,
+  },
+
+  -- DAP
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    config = function()
+      require("mason-nvim-dap").setup({
+        ensure_installed = { "codelldb" },
+        automatic_installation = true,
+      })
+    end,
   },
   {
     "mfussenegger/nvim-dap",
-    optional = true,
-    dependencies = {
-      -- Ensure C/C++ debugger is installed
-      "williamboman/mason.nvim",
-      optional = true,
-      opts = function(_, opts)
-        if type(opts.ensure_installed) == "table" then
-          vim.list_extend(opts.ensure_installed, { "codelldb" })
-        end
-      end,
+    keys = {
+      { "<F5>", function() require("dap").continue() end, desc = "DAP continue" },
+      { "<F10>", function() require("dap").step_over() end, desc = "DAP step over" },
+      { "<F11>", function() require("dap").step_into() end, desc = "DAP step into" },
+      { "<F12>", function() require("dap").step_out() end, desc = "DAP step out" },
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "DAP toggle breakpoint" },
     },
-    opts = function()
-    local dap = require("dap")
-    if not dap.adapters["codelldb"] then
-      require("dap").adapters["codelldb"] = {
+    config = function()
+      local dap = require("dap")
+      local mason_registry = require("mason-registry")
+
+      local codelldb_path = "codelldb"
+      local liblldb_path = nil
+
+      if mason_registry.has_package("codelldb") then
+        local pkg = mason_registry.get_package("codelldb")
+        local install_path = pkg:get_install_path()
+        codelldb_path = install_path .. "/extension/adapter/codelldb"
+
+        if vim.fn.has("mac") == 1 then
+          liblldb_path = install_path .. "/extension/lldb/lib/liblldb.dylib"
+        elseif vim.fn.has("win32") == 1 then
+          liblldb_path = install_path .. "/extension/lldb/bin/liblldb.dll"
+        else
+          liblldb_path = install_path .. "/extension/lldb/lib/liblldb.so"
+        end
+      end
+
+      dap.adapters.codelldb = {
         type = "server",
-        host = "localhost",
         port = "${port}",
         executable = {
-          command = "codelldb",
-          args = {
-            "--port",
-            "${port}",
+          command = codelldb_path,
+          args = { "--port", "${port}" },
+          detached = false,
+        },
+      }
+
+      if liblldb_path then
+        dap.adapters.codelldb.env = {
+          LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES",
+        }
+      end
+
+      for _, lang in ipairs({ "c", "cpp" }) do
+        dap.configurations[lang] = {
+          {
+            name = "Launch file",
+            type = "codelldb",
+            request = "launch",
+            program = function()
+              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+            stopOnEntry = false,
           },
-        },
-      }
-    end
-    for _, lang in ipairs({ "c", "cpp" }) do
-      dap.configurations[lang] = {
-        {
-          type = "codelldb",
-          request = "launch",
-          name = "Launch file",
-          program = function()
-            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-          end,
-          cwd = "${workspaceFolder}",
-        },
-        {
-          type = "codelldb",
-          request = "attach",
-          name = "Attach to process",
-          pid = require("dap.utils").pick_process,
-          cwd = "${workspaceFolder}",
-        },
-      }
-    end
-  end,
+          {
+            name = "Attach to process",
+            type = "codelldb",
+            request = "attach",
+            pid = require("dap.utils").pick_process,
+            cwd = "${workspaceFolder}",
+          },
+        }
+      end
+    end,
   },
+
+  -- Markdown rendering
   {
-    "MeanderingProgrammer/markdown.nvim",
-    name = "render-markdown",
-    dependencies = {"nvim-treesitter/nvim-treesitter"},
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
     config = function()
       require("render-markdown").setup({
         start_enabled = true,
       })
     end,
-  }
+  },
 }
